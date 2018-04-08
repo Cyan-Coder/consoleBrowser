7544b139b4703800c2e318b56670cf89219fad63
Damian Dassen <damian.dassen.dd@gmail.com>
2018-04-08 18:40:41 +0200

    Update tag.h

--- tag.h
+++ tag.h
@@ -20,17 +20,72 @@ enum colour {
 	WHITE = 0x000F,
 };	
 
+//base class for every tag
 class tag {
+	//definition e.g. <p>
 	std::string m_definition;
+	//colour of the characters on page
 	colour m_fontColour = WHITE;
-	colour m_backColour = BLACK;
+	//colour of the background of the characters
+	colour m_backColour;
+	//unsigned integer that states how many tags are nested within
+	unsigned int m_howManyNested;
+public:
+	//getters
+	colour getFontColour() {
+		//returns the font colour
+		return m_fontColour;
+	};
+
+	colour getBackColour() {
+		//returns the background colour
+		return m_backColour;
+	};
+
+	unsigned int getHowManyNested() {
+		//returns how many tags are nested within
+		return m_howManyNested;
+	};
+
+	//setters
+	void setFontColour(colour input) {
+		//sets the font colour
+		m_fontColour = input;
+	};
+
+	void setBackColour(colour input) {
+		//set the background colour
+		m_backColour = input;
+	};
+
+	void setHowManyNested(unsigned int input) {
+		//sets how many nested tags are within
+		m_howManyNested = input;
+	};
 };
 
+//a text tag is a tag that only contains text and font colours
 class textTag : public tag {
-	std::string contents;
+	//the contents of the text in this case the text written between the two tags
+	std::string m_contents;
+
+	//constructor of the textTag
+	textTag(colour fontColour, colour backColour)
+	{
+		setFontColour(fontColour);
+		setBackColour(backColour);
+	}
 };
 
+//link tag is a tag that contains text, a hyperlink and font colours
 class linkTag : public tag {
-	std::string link;
-	std::string name;
+	//hyperlink
+	std::string m_link;
+	//content contains the text between the two tags
+	std::string m_content;
+	//constructor of the linkTag
+	linkTag(colour fontColour, colour backColour){
+		setFontColour(fontColour);
+		setBackColour(backColour);
+	}
 };
