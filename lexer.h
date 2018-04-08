b9c367cda74e829b10f09e9aa35f0afcf7f80a89
Damian Dassen <damian.dassen.dd@gmail.com>
2018-04-08 18:38:56 +0200

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -5,17 +5,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "tag.h"
 
+//the class of the lexer
 class lexer {
+	//public members
 public:
+	//definition of the constructor
 	lexer(std::vector<char> source);
-
+	
+	//definitions of the members
+	int offset;
+	std::string current;
 	std::string buffer;
 	std::vector<char> source;
-	std::vector<int> lexedFile;
-	std::vector<char> lexedText;
-	std::vector<std::string> textConcatted;
-	bool wasChar = false;
+	std::vector<std::string> structuredFile;
+	bool isInTag = false;
 	bool skipLine = false;
 	void lexFile();
+	std::vector<std::string> getStructeredFile();
 };
